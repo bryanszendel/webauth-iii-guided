e29e3312744fedb8f022cd67d1c53bd1a97c3177
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-18 10:30:51 -0600

    added token responses to /login and /register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -11,7 +12,8 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      const token = generateToken(user)
+      res.status(201).json({saved, token});
     })
     .catch(error => {
       res.status(500).json(error);
@@ -25,8 +27,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +40,17 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  }
+  const secret = 'keep it secret, keep it safe!'
+  const options = {
+    expiresIn: '1d',
+  }
+
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
